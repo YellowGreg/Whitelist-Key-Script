cf27af1d63cd9f6ef86509ebaea29974b84a46e7
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-12 13:24:44 -0500

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -3,7 +3,7 @@
 local key = _G.Key -- Put Key Here
 local check = "checkhere?key=" .. key -- Check The Key If its Right or Wrong
 if game:HttpGet(check) == "Whitelisted" then -- After Done Checking The Right Key it will give you your script
-  loadstring(game:HttpGet("Script-Goes-Here"))()
+  loadstring(game:HttpGet("Script-Goes-Here"))() -- You can put any script here 
 else
   game.Players.LocalPlayer:Kick("Invalid Key!") -- Kick Messege(You can change this)
 end
